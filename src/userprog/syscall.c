afebe5cfefb3c3c6d40ef73ae4b4c6c6b12bc98c
Eli Gilbert <eg1923@ic.ac.uk>
2024-10-29 10:41:16 +0000

    Feature: Implemented the argument passing quickfix. Setup a verify function to ensure safe user memory access.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,6 +6,14 @@
 
 static void syscall_handler (struct intr_frame *);
 
+/* Used to ensure safe memory access by verifying a pointer pre-dereference.
+Should certainly be called with interrupts off, or will be useless. */
+static bool
+verify (void *vaddr) {
+  ASSERT (intr_get_level() == INTR_OFF);
+  return is_user_vaddr(vaddr) && pagedir_get_page(vaddr);
+}
+
 void
 syscall_init (void) 
 {
