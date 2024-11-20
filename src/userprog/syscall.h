7462514853edf0f9f21f0640caf22854c74974d7
Eli Gilbert <eg1923@ic.ac.uk>
2024-11-20 01:11:16 +0000

    Refactor: Refactored the syscall handler to make use of function pointers  and the extract_arg_n functions as macros.

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,5 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
-#include "../src/lib/user/syscall.h"
 #include "../src/threads/vaddr.h"
 #include "../src/filesys/filesys.h"
 #include "../src/filesys/file.h"
